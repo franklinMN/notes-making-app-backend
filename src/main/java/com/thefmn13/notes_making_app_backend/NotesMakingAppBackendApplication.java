b84778f392f6a5f341b61057cbdc3b5d22b71868
franklinMN <frankmnwinter@gmail.com>
2024-09-27 22:29:56 +0530

    installation commit

---
+++ src/main/java/com/thefmn13/notes_making_app_backend/NotesMakingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thefmn13.notes_making_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesMakingAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesMakingAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thefmn13/notes_making_app_backend/NotesMakingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thefmn13.notes_making_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesMakingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
